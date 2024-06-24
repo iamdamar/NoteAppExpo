640514f337e44a6f54f519cc2efe3d7119e41d56
damarwu <hello.damarwu@gmail.com>
2024-06-24 10:06:35 +0700

    second commit

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'
+import Home from './src/pages/home'
+import AddNote from './src/pages/addNote'
+import EditNote from './src/pages/editNote'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const CurrentPageWidget = ({currentPage, noteList, setCurrentPage, addNote, editNote, deleteNote}) => {
+    switch (currentPage.page) {
+        case 'home':
+            return (
+                <Home
+                    noteList={noteList}
+                    setCurrentPage={setCurrentPage}
+                    deleteNote={deleteNote}
+                />
+            )
+        case 'add':
+            return <AddNote 
+                    setCurrentPage={setCurrentPage}
+                    addNote={addNote} />
+        case 'edit':
+            return <EditNote
+                    setCurrentPage={setCurrentPage}
+                    editNote={editNote}
+                    note={currentPage.note} />
+        default:
+            return <Home />
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+    const [currentPage, setCurrentPage] = useState({page: 'home'})
+
+    const [noteList, setNoteList] = useState([
+        {
+            id: 1,
+            title: 'Note pertama',
+            desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry'
+        },
+    ])
+
+    const addNote = (title, desc) => {
+        const id = noteList.length > 0 ? noteList[noteList.length - 1].id + 1 : 1
+    
+        setNoteList([
+            ...noteList,
+            {
+                id,
+                title: title,
+                desc: desc,
+            },
+        ])
+    }
+
+    const editNote = (id, title, desc) => {
+        setNoteList(noteList.map(note =>
+            note.id === id ? { ...note, title, desc} : note
+        ))
+    }
+
+    const deleteNote = (id) => {
+        setNoteList(noteList.filter(note => note.id !== id))
+    }
+
+    return (
+        <CurrentPageWidget
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            noteList={noteList}
+            addNote={addNote}
+            editNote={editNote}
+            deleteNote={deleteNote}
+        />
+    )
+}
+
+export default App
